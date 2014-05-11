641fdfffebb82b2fe43ab354bd0504ac3c5c1527
David Buckhurst <dbuckhurst@inview.co.uk>
2014-05-11 21:33:08 +0100

    Very basic proxy

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-proximon
-========
+# proximon
 
-Playing with proxying
+What's really involved in building a proxy? 
+
+Is it really that complicated?
+
+How would you build a fast proxy in ruby?
+
---
+++ proximon.rb
@@ -0,0 +1,28 @@
+require 'sinatra'
+require 'open-uri'
+require 'sinatra/reloader'
+
+set :port => 4646
+
+# Monkeypatch OpenURI to allow http -> https redirection
+module OpenURI
+  def OpenURI.redirectable?(uri1, uri2)
+    uri1.scheme.downcase == uri2.scheme.downcase ||
+    (/\A(?:http|ftp)\z/i =~ uri1.scheme && /\A(?:http|ftp)\z/i =~ uri2.scheme) ||
+    ( uri1.scheme.downcase == 'http' && uri2.scheme.downcase == 'https' )
+  end
+end
+
+get '/*' do
+  url = request.url
+  puts "Pass through for request " + request.url.to_s
+
+  # Set the content type to whatever was requested
+  content_type request.preferred_type.to_s
+
+  open(url) do |content|
+    content.read
+  end
+end
+
+
